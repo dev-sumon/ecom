5556b0842e780dd33f9b879ea68f8896fbce1201
dev-sumon <akhtaruzzamansumon7@gmail.com>
2023-11-01 12:27:23 +0600

    Update

--- app/Http/Controllers/Admin/CategoryController.php
+++ app/Http/Controllers/Admin/CategoryController.php
@@ -38,4 +38,43 @@ class CategoryController extends Controller
         $category->save();
         return redirect()->route('product.category.index')->withStatus(__('Category Created Successfully'));
     }
+    function view($id): View
+    {
+        $s['category'] = Category::with(['createdBy','updatedBy'])->where('id',$id)->first();
+        return view('admin.product.category.view',$s);
+    }
+
+    function edit($id): View
+    {
+        $s['category'] = Category::findOrFail($id);
+        return view('admin.product.category.edit',$s);
+    }
+    function update(CategoryRequest $req, $id): RedirectResponse
+    {
+        $category = Category::findOrFail($id);
+
+        if($req->hasFile('image')){
+            $image = $req->file('image');
+            $path = $image->store('product/category','public');
+            $this->fileDelete($category->image);
+            $category->image = $path;
+        }
+        $category->slug = Str::slug($req->name);
+        $category->name = $req->name;
+        $category->updated_by = auth()->user()->id;
+
+        $category->save();
+        return redirect()->route('product.category.index')->withStatus(__('Category Updated Successfully'));
+    }
+    function status($id){
+        $category = Category::findOrFail($id);
+        $this->changeStatus($category);
+        return redirect()->back()->withStatus(__('Category Status Change Successfully'));;
+    }
+    function delete($id){
+        $category = Category::findOrFail($id);
+        $this->fileDelete($category->image);
+        $category->delete();
+        return redirect()->route('product.category.index')->withStatus(__('Category Deleted Successfully'));;
+    }
 }
--- resources/views/admin/product/category/view.blade.php
+++ resources/views/admin/product/category/view.blade.php
@@ -1,14 +1,14 @@
 @extends('admin.layout.master')
-@section('title', 'View')
+@section('title', 'Category View')
 @section('content')
     <div class="">
         <div class="row">
             <div class="col-md-12 col-sm-12">
                 <div class="x_panel">
                     <div class="x_title">
-                        <h2>View User</h2>
+                        <h2>View Category</h2>
                         <ul class="nav navbar-right panel_toolbox">
-                            <a class="btn btn-primary btn-sm" href="{{ route('user.index') }}">{{ __('Back') }}</a>
+                            <a class="btn btn-primary btn-sm" href="{{ route('product.category.index') }}">{{ __('Back') }}</a>
                             <li><a class="collapse-link"><i class="fa fa-chevron-up"></i></a>
                             </li>
                         </ul>
@@ -25,77 +25,46 @@
                                             <tr>
                                                 <th>Name</th>
                                                 <td>:</td>
-                                                <td>{{$user->name}}</td>
-                                            </tr>
-                                            <tr>
-                                                <th>Cover Image</th>
-                                                <td>:</td>
-                                                <td><img src="{{ asset('storage/' . $user->cover_image) }}"
-                                                    alt="{{ __('Cover Photo') }}" height="40px" width="40px"></td>
+                                                <td>{{$category->name}}</td>
                                             </tr>
                                             <tr>
                                                 <th>Image</th>
                                                 <td>:</td>
-                                                <td><img src="{{ asset('storage/' . $user->image) }}"
-                                                    alt="{{ __('Cover Photo') }}" height="40px" width="40px"></td>
+                                                <td><img src="{{ asset('storage/' . $category->image) }}"
+                                                    alt="{{ __('Product Image') }}" height="40px" width="40px"></td>
                                             </tr>
                                             <tr>
-                                                <th>Role</th>
+                                                <th>Slug</th>
                                                 <td>:</td>
-                                                <td>{{$user->role}}</td>
-                                            </tr>
-                                            <tr>
-                                                <th>Email</th>
-                                                <td>:</td>
-                                                <td>{{$user->email}}</td>
+                                                <td>{{$category->slug}}</td>
                                             </tr>
                                             <tr>
                                                 <th>Status</th>
                                                 <td>:</td>
                                                 <td>
-                                                    <span class="badge {{$user->status == 1 ? 'badge-success' : 'badge-warning'}}">{{$user->status == 1 ? 'Active' : 'Deactive'}}</span>
+                                                    <span class="badge {{$category->status == 1 ? 'badge-success' : 'badge-warning'}}">{{$category->status == 1 ? 'Active' : 'Deactive'}}</span>
                                                 </td>
                                             </tr>
-                                            @if(is_array(json_decode($user->billing_address,true)))
-                                                @foreach (json_decode($user->billing_address) as $key=>$address)
-                                                    <tr>
-                                                        <th>Billing Address-{{$key}}</th>
-                                                        <td>:</td>
-                                                        <td>{{$address->billing}}</td>
-                                                    </tr>
-                                                @endforeach
-                                            @else
-                                                <tr>
-                                                    <th>Billing Address</th>
-                                                    <td>:</td>
-                                                    <td>NULL</td>
-                                                </tr>
-                                            @endif
-
-                                            @if(is_array(json_decode($user->shipping_address,true)))
-                                                @foreach (json_decode($user->shipping_address) as $key=>$address)
-                                                    <tr>
-                                                        <th>Shipping Address-{{$key}}</th>
-                                                        <td>:</td>
-                                                        <td>{{$address->shipping}}</td>
-                                                    </tr>
-                                                @endforeach
-                                            @else
-                                                <tr>
-                                                    <th>Shipping Address</th>
-                                                    <td>:</td>
-                                                    <td>NULL</td>
-                                                </tr>
-                                            @endif
+                                            <tr>
+                                                <th>Created By</th>
+                                                <td>:</td>
+                                                <td>{{$category->createdBy->name}}</td>
+                                            </tr>
+                                            <tr>
+                                                <th>Updated By</th>
+                                                <td>:</td>
+                                                {{-- <td>{{$category->updated_by ? $category->updatedBy->name : 'N/A'}}</td> --}}
+                                                <td>{{ optional($category->updatedBy)->name}}</td>
+                                            </tr>
                                             <tr>
                                                 <th>Created At</th>
                                                 <td>:</td>
-                                                <td>{{date('d-M-Y', strtotime($user->created_at))}}</td>
+                                                <td>{{date('d-M-Y', strtotime($category->created_at))}}</td>
                                             </tr>
                                             <tr>
                                                 <th>Updated At</th>
                                                 <td>:</td>
-                                                <td>{{($user->created_at != $user->updated_at) ? (date('d-M-Y', strtotime($user->updated_at))) : "N/A" }}</td>
+                                                <td>{{($category->created_at != $category->updated_at) ? (date('d-M-Y', strtotime($category->updated_at))) : "N/A" }}</td>
                                             </tr>
                                         </tbody>
                                     </table>
